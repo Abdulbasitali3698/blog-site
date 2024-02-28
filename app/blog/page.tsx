b88df2d5faa6151fa7d08806d2edac305620282c
Abdulbasitali3698 <43988292+Abdulbasitali3698@users.noreply.github.com>
2024-02-28 23:25:35 +0500

    Blog-site

---
+++ app/blog/page.tsx
@@ -0,0 +1,42 @@
+export default function Home() {
+  return (
+    <div>
+        <nav className="navbar bg-light">
+            <div className="container">
+                <div>
+                <a href="#" className="navbar-brand">Navbar</a>
+                </div>
+                <div >
+                    <label htmlFor="userName">Basit</label>
+                </div>
+            </div>
+        </nav>
+        <div className="container">
+            <div className="card offset col-8">
+                <div className="card-header">
+                    <h3>Title</h3>
+                </div>
+                <div className="card-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laboriosam, asperiores assumenda dolorem labore explicabo suscipit aspernatur ut nostrum reiciendis repellat doloribus a alias reprehenderit cumque odio distinctio culpa. Commodi.</p>
+                </div>
+            </div>
+            <div className="card offset col-8">
+                <div className="card-header">
+                    <h3>Title</h3>
+                </div>
+                <div className="card-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laboriosam, asperiores assumenda dolorem labore explicabo suscipit aspernatur ut nostrum reiciendis repellat doloribus a alias reprehenderit cumque odio distinctio culpa. Commodi.</p>
+                </div>
+            </div>
+            <div className="card offset col-8">
+                <div className="card-header">
+                    <h3>Title</h3>
+                </div>
+                <div className="card-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laboriosam, asperiores assumenda dolorem labore explicabo suscipit aspernatur ut nostrum reiciendis repellat doloribus a alias reprehenderit cumque odio distinctio culpa. Commodi.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
